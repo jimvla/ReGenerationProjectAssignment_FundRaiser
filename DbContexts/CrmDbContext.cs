2ac6b79af0462b2c2cc1e58ffb202d47f8e748d6
traba49 <ptrabak@gmail.com>
2023-07-05 11:54:05 +0300

    Add project files.

---
+++ DbContexts/CrmDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReGenerationProjectAssignment_FundRaiser.DbContexts
+{
+    public class CrmDbContext : DbContext
+    {
+        /*public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Payment> Payments { get; set; }*/
+
+        public CrmDbContext() { }
+        public CrmDbContext(DbContextOptions<CrmDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReGenerationProjectAssignment_FundRaiser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
